1307949910c50860aca5d8799d36fe03c0b5109f
Ken Lu <ken@seeice.com>
2013-04-29 08:20:55 -0400

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TagWithDesc::Application.config.secret_key_base = '859b892ed1169c5c69e8ad2c3f553ab421ce9440b0a50124e8d3ed238766a82bf76671f0d52526edd75556ddb8af8c89516ae950fa19c06cf27a77d17e36e925'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TagWithDesc::Application.config.session_store :encrypted_cookie_store, key: '_tag_with_desc_session'
