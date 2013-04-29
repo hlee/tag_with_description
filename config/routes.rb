da0a32bef9e34566a6c702db6ac5ca82c2538c93
Ken Lu <ken@seeice.com>
2013-04-29 08:45:48 -0400

    Revert "basic question scaffold"
    This reverts commit df590d0421c397bef1d4e182060075f92c74fdec.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 TagWithDesc::Application.routes.draw do
-  resources :questions
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
